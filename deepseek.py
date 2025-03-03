df2fea637163f737f7f9041477e5a90362c8f5a2
Lrcxzz <Lrcxzz@163.com>
2025-03-03 18:07:25 +0800

    源码
    源码

---
+++ deepseek.py
@@ -0,0 +1,12 @@
+from time import sleep
+while True:
+    print('I am DeepSeek,Can I help you?')
+    inp = input('please input,Enter 0 to exit')
+    if inp == '0':
+        break
+    else:
+        print('The server is busy,please try again later')
+        print('Write a conversation that is about to begin')
+        sleep(1.145)
+        print('\n')
+        print('\n')
